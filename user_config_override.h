4b1986b9b63e8e49da475d6bc95d2c977e1d85ce
sonniboi <131561939+sonniboi@users.noreply.github.com>
2026-03-26 17:52:21 +0100

    Add SML user_config_override.h

---
+++ user_config_override.h
@@ -0,0 +1,15 @@
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+
+// SML support (Hichi Smartmeter / IR-Lesekopf)
+#ifndef USE_SCRIPT
+  #define USE_SCRIPT
+#endif
+#ifndef USE_SML_M
+  #define USE_SML_M
+#endif
+#ifdef USE_RULES
+  #undef USE_RULES
+#endif
+
+#endif  // _USER_CONFIG_OVERRIDE_H_
